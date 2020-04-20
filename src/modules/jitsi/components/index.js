822bd6ccc93f0b65de6bdf2586a2d8132e1d50ca
Katsiaryna <K.Krasilnikova@itechart-group.com>
2020-04-20 18:35:20 +0300

    Added logic with jitsi

--- src/modules/jitsi/components/index.js
+++ src/modules/jitsi/components/index.js
@@ -1,12 +1,58 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import { Jutsu } from "react-jutsu";
 
 import styles from "./styles";
 
-const Jitsi = ({ classes, currentUser }) => {
+const Jitsi = ({
+  classes,
+  call,
+  room,
+  name,
+  password,
+  handleClick,
+  setRoom,
+  setName,
+  setPassword,
+}) => {
   return (
     <div className={classes.wrapper}>
       <h2>Jitsi Page</h2>
+      {call ? (
+        <Jutsu
+          roomName={room}
+          userName={name}
+          password={password}
+          loadingComponent={<p>loading ...</p>}
+        />
+      ) : (
+        <form>
+          <input
+            id="room"
+            type="text"
+            placeholder="Room"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          <input
+            id="name"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            id="password"
+            type="text"
+            placeholder="Password (optional)"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClick} type="submit">
+            Start / Join
+          </button>
+        </form>
+      )}
     </div>
   );
 };
--- src/modules/jitsi/containers/JitsiContainer.js
+++ src/modules/jitsi/containers/JitsiContainer.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Jitsi from "../components";
 
 const JitsiContainer = ({ currentUser }) => {
-  // const [uploads, setUploads] = useState([]);
-  // const [counter, setCounter] = React.useState(0);
+  const [room, setRoom] = useState("");
+  const [name, setName] = useState("");
+  const [call, setCall] = useState(false);
+  const [password, setPassword] = useState("");
 
-  // const uploadFiles = async (e) => {
-  // };
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (room && name) setCall(true);
+  };
 
-  return <Jitsi currentUser={currentUser} />;
+  return (
+    <Jitsi
+      call={call}
+      room={room}
+      name={name}
+      password={password}
+      handleClick={handleClick}
+      setRoom={setRoom}
+      setName={setName}
+      setPassword={setPassword}
+    />
+  );
 };
 
 export default JitsiContainer;
